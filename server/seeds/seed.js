0a49cd27f450c813a20bc6645fceb48f9ffe8faa
Nicholas Moser <nicholas.r.moser@gmail.com>
2023-04-06 21:07:24 -0600

    seeds.js

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,12 +1,13 @@
 const db = require('../config/connection');
-const { Tech } = require('../models');
+const { Post, User } = require('../models');
 
 const techData = require('./techData.json');
 
 db.once('open', async () => {
   await Tech.deleteMany({});
 
-  const technologies = await Tech.insertMany(techData);
+  const technologies = await User.insertMany(techData);
+  const posts = await Post.insertMany(techData);
 
   console.log('Technologies seeded!');
   process.exit(0);
