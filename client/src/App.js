fd9ef290c6e7ec2b949566d2e043e2a04fce9bc6
Hans Shen <hshen801@gmail.com>
2023-04-06 21:11:14 -0600

    edited App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Nav from './components/Nav';
-//import PostList from './components/PostList.js';
 import Post from './components/Post';
 import Login from './components/Login';
 import Signup from './components/Signup';
@@ -15,7 +14,6 @@ function App() {
     <Router>
       <Nav />
       <Switch>
-        <Route exact path="/PostList" component={PostList} />
         <Route exact path="/Post/" component={Post} />
         <Route exact path="/Login" component={Login} />
         <Route exact path="/Signup" component={Signup} />
