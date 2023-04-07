c3d96ea792dbca1e5b01fd5244cd0303f2e3b420
Hans Shen <hshen801@gmail.com>
2023-04-06 20:10:10 -0600

    Edited a little bit in App.js to build out frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Nav from './components/Nav';
+//import PostList from './components/PostList.js';
+import Post from './components/Post';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import User from './components/User';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Nav />
+      <Switch>
+        <Route exact path='/PostList' component={PostList} />
+        <Route exact path='/Post/' component={Post} />
+        <Route exact path='/Login' component={Login} />
+        <Route exact path='/Signup' component={Signup} />
+        <Route exact path='/User' component={User} />
+      </Switch>
+    </Router>
   );
 }
 
