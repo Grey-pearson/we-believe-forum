03626294030bfd7128825a2871ac0aca7bfce40d
Hans Shen <hshen801@gmail.com>
2023-04-12 20:45:07 -0600

    Changed styling on Login.js, and got Profile.js to render

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,32 +1,70 @@
 import React, { useState } from 'react';
+import styles from './Login.module.css';
 
 function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
 
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-  
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      // handle login logic
-    };
-  
-    return (
-      <div className="login">
-        <h1>Login</h1>
-        <form onSubmit={handleSubmit}>
-          <label>
-            Username:
-            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-          </label>
-          <label>
-            Password:
-            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-          </label>
-          <button type="submit">Login</button>
-        </form>
-      </div>
-    );
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // handle login logic
+  };
 
-  export default Login;
+  return (
+    <div className={styles.container}>
+      <h1>Login</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <label className={styles.label}>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <label className={styles.label}>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button type="submit">Login</button>
+      </form>
+      <br />
+      <br />
+      <h1>Register</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <label className={styles.label}>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <label className={styles.label}>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <label>
+          E-mail:
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
 
+export default Login;
