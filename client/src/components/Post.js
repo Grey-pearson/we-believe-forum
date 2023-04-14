35148c1e0c35994fc502404b3d840fb42f3de42f
Grey P <greytpearson@gmail.com>
2023-04-13 19:43:38 -0600

    nav test

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -12,6 +12,7 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Paper from '@mui/material/Paper';
 
 function Post(props) {
   const { postText, postAuthor, createdAt, comments } = props
